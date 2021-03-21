ca1d1aa8306f3ad3518bc80c8babde18bf9f03ae
Berkan Aslan <aslnberkan@gmail.com>
2021-03-21 17:38:52 +0300

    App running successfully.

--- src/main/java/com/berkanaslan/springsecurityexample/config/SpringSecurityExampleConfig.java
+++ src/main/java/com/berkanaslan/springsecurityexample/config/SpringSecurityExampleConfig.java
@@ -17,7 +17,7 @@ public class SpringSecurityExampleConfig {
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
-        viewResolver.setPrefix("WEB-INF/view");
+        viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Spring Security Example Project</title>
+    </head>
+    <body>
+        <h2>X Company Home Page</h2>
+        <hr>
+        Welcome to the our company home page!
+    </body>
+</html>
